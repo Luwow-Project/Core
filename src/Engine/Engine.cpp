fba9e2a67e1df18958fc25c685c427c3c8cf31f1
TenebrisNoctua <172976441+TenebrisNoctua@users.noreply.github.com>
2026-03-30 23:54:27 +0300

    Remove currentState, and fix debugger callback.

--- src/Engine/Engine.cpp
+++ src/Engine/Engine.cpp
@@ -61,7 +61,6 @@ void Engine::initialize() {
 
     luaL_openlibs(mainState);
     initializeRequire();
-    currentState = lua_newthread(mainState);
 }
 
 static int static_require(lua_State* L) {
@@ -82,7 +81,7 @@ void Engine::initializeRequire() {
 }
 
 int Engine::executeModule(lua_State* L, const std::string& chunkName, const std::string& bytecode) {
-    lua_State* T = currentState;
+    lua_State* T = lua_newthread(mainState);
     luaL_sandboxthread(T);
 
     int result = luau_load(T, chunkName.c_str(), bytecode.data(), bytecode.size(), 0);
@@ -93,7 +92,7 @@ int Engine::executeModule(lua_State* L, const std::string& chunkName, const std:
     }
 
     if (usesDebuggerNewLuauCallback) {
-        debuggerNewLuauCallback(T, chunkName, false);
+        debuggerNewLuauCallback(T, chunkName, true);
     }
 
     int status = lua_resume(T, nullptr, 0);
--- src/Engine/Engine.h
+++ src/Engine/Engine.h
@@ -37,10 +37,8 @@ public:
     void run();
 
     lua_State* getMainState() { return mainState; };
-    lua_State* getCurrentState() { return currentState; };
 private:
     lua_State* mainState;
-    lua_State* currentState;
 
     bool usesCompiler;
     CompilerCallbackType compilerCallback;
